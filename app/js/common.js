a6f5a628e40562a35316655e25ad655b4dd67149
Alex Gashkov <gashkov321@gmail.com>
2019-06-20 23:32:32 +0300

    mobile version done

--- app/js/common.js
+++ app/js/common.js
@@ -1,22 +1,50 @@
- 
-//fancybox init
- $('.microblog-video a').fancybox({
-  caption : function( instance, item ) {
-    return $(this).parent().find('.card-text').html();
-  }
-});
+$(document).ready(function() {
+    //fancybox init
+    $('.microblog-video a').fancybox({
+     	caption: function(instance, item) {
+     		return $(this).parent().find('.card-text').html();
+     	}
+     });
 
 
-//tooltip init
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+    //tooltip init
+    $(function() {
+     	$('[data-toggle="tooltip"]').tooltip()
+     })
 
-//mobile menu
-$('.menu-btn').on('click', function() {
-	$('.menu').addClass('is-active');
-});
+    //mobile menu
+    $('.menu-btn').on('click', function() {
+     	$('.menu').addClass('is-active');
+    });
 
-$('.menu__close').on('click', function() {
-	$('.menu').removeClass('is-active');
+    $('.menu__close').on('click', function() {
+     	$('.menu, .sub-nav').removeClass('is-active');
+    });
+
+     
+
+    //resize function
+    $(window).on("resize", function(e) {
+     	checkScreenSize();
+    });
+
+    checkScreenSize();
+
+    function checkScreenSize() {
+     	var newWindowWidth = $(window).width();
+     	if (newWindowWidth < 481) {
+     		$('.logo').on('click', function() {
+		     	function stopDefAction(evt) {
+		     		evt.preventDefault();
+		     	}
+		     	$('.sub-nav').addClass('is-active');
+		    });
+     	} 
+    }
+
+     //account lift-up
+    $('.lift-up__btn').on('click', function() {
+     	$('.lift-up').toggleClass('lift-up_active');
+    });
+
 });
